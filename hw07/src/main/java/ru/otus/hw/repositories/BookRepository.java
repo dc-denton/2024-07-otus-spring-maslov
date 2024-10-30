f1da6f7c3abe31829b1e2d3d9e8b410a430dd4b1
Pavel Maslov <p.maslov@x5.ru>
2024-10-30 11:14:40 +0300

    hw07 fixes

--- hw07/src/main/java/ru/otus/hw/repositories/BookRepository.java
+++ hw07/src/main/java/ru/otus/hw/repositories/BookRepository.java
@@ -8,9 +8,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-    @EntityGraph(value = "book-entity-graph")
+    @EntityGraph(value = "book-author-genres-entity-graph")
     Optional<Book> findById(long id);
 
-    @EntityGraph(value = "book-author-genres-entity-graph")
+    @EntityGraph(value = "book-entity-graph")
     List<Book> findAll();
 }
